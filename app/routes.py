f1976472925f016576f1d8f4b87035df22041a69
Cesar Gaspar <rasec.rapsag@gmail.com>
2020-07-17 14:38:07 -0300

    Part 02 - Adicionamos a estrutura de templates ao projeto. Também passamos variáveis para a view e trabalhamos com elas usando if e for.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Cesar'}
+    posts = [
+        {
+            'author': {'username': 'Gaspar'},
+            'body': 'Maravilhoso dia em São Paulo!'
+        },
+        {
+            'author': {'username': 'Augusto'},
+            'body': 'O filme do Dead Pool é muito bom!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
